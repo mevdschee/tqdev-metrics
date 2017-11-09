738109151d4e701507bd272312ef0153ae3f7daf
Maurits van der Schee <maurits@vdschee.nl>
2017-11-09 01:07:04 +0100

    Some first tests added and some cleanup

---
+++ metrics-core/src/test/java/com/tqdev/metrics/core/MetricRegistryTests.java
@@ -0,0 +1,211 @@
+package com.tqdev.metrics.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetricRegistryTests {
+
+	final MetricRegistry registry = MetricRegistry.getInstance();
+
+	@Before
+	public void initialize() {
+		registry.reset();
+	}
+
+	@Test
+	public void shouldNotHaveTypesWhenCreated() {
+		assertThat(registry.getTypes().iterator().hasNext()).isEqualTo(false);
+	}
+
+	@Test
+	public void shouldHaveTypeAndKeyWhenIncrementing() {
+		registry.increment("type", "key");
+		assertThat(registry.getTypes().iterator().next()).isEqualTo("type");
+		assertThat(registry.getKeys("type").iterator().next()).isEqualTo("key");
+	}
+
+	@Test
+	public void shouldBeZeroWhenIncrementingAndDecrementing() {
+		registry.increment("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(1);
+		registry.decrement("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(0);
+	}
+
+	@Test
+	public void shouldBeZeroWhenDecrementingAndIncrementing() {
+		registry.decrement("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(-1);
+		registry.increment("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(0);
+	}
+
+	@Test
+	public void shouldHaveTypeAndKeyWhenAdding() {
+		registry.add("type", "key", 123);
+		assertThat(registry.getTypes().iterator().next()).isEqualTo("type");
+		assertThat(registry.getKeys("type").iterator().next()).isEqualTo("key");
+	}
+
+	@Test
+	public void shouldBeZeroWhenAddingNumberPositiveAndNegative() {
+		registry.add("type", "key", 123);
+		assertThat(registry.get("type", "key")).isEqualTo(123);
+		registry.add("type", "key", -123);
+		assertThat(registry.get("type", "key")).isEqualTo(0);
+	}
+
+	@Test
+	public void shouldBeZeroWhenAddingNumberNegativeAndPositive() {
+		registry.add("type", "key", -123);
+		assertThat(registry.get("type", "key")).isEqualTo(-123);
+		registry.add("type", "key", 123);
+		assertThat(registry.get("type", "key")).isEqualTo(0);
+	}
+
+	@Test
+	public void shouldBeMinLongWhenIncrementingMaxLong() {
+		registry.set("type", "key", Long.MAX_VALUE);
+		registry.increment("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(Long.MIN_VALUE);
+	}
+
+	@Test
+	public void shouldBeMaxLongWhenDecrementingMinLong() {
+		registry.set("type", "key", Long.MIN_VALUE);
+		registry.decrement("type", "key");
+		assertThat(registry.get("type", "key")).isEqualTo(Long.MAX_VALUE);
+	}
+
+	@Test
+	public void shouldBeMinLongWhenAddingOneToMaxLong() {
+		registry.add("type", "key", Long.MAX_VALUE);
+		registry.add("type", "key", 1);
+		assertThat(registry.get("type", "key")).isEqualTo(Long.MIN_VALUE);
+	}
+
+	@Test
+	public void shouldBeMaxLongWhenAddingMinusOneToMinLong() {
+		registry.add("type", "key", Long.MIN_VALUE);
+		registry.add("type", "key", -1);
+		assertThat(registry.get("type", "key")).isEqualTo(Long.MAX_VALUE);
+	}
+
+	@Test
+	public void shouldNotHaveTypesWhenReset() {
+		registry.increment("type", "key");
+		registry.reset();
+		assertThat(registry.getTypes().iterator().hasNext()).isEqualTo(false);
+	}
+
+	@Test
+	public void shouldHaveTypeAndKeyWhenSettingLong() {
+		registry.set("type", "key", 123);
+		assertThat(registry.getTypes().iterator().next()).isEqualTo("type");
+		assertThat(registry.getKeys("type").iterator().next()).isEqualTo("key");
+	}
+
+	@Test
+	public void shouldHaveTypeAndKeyWhenSettingGauge() {
+		registry.set("type", "key", () -> 123);
+		assertThat(registry.getTypes().iterator().next()).isEqualTo("type");
+		assertThat(registry.getKeys("type").iterator().next()).isEqualTo("key");
+	}
+
+	@Test
+	public void shouldBeNumberWhenSettingLongNumber() {
+		registry.set("type", "key", 123);
+		assertThat(registry.get("type", "key")).isEqualTo(123);
+	}
+
+	@Test
+	public void shouldBeNumberWhenSettingGaugeNumber() {
+		registry.set("type", "key", () -> 123);
+		assertThat(registry.get("type", "key")).isEqualTo(123);
+	}
+
+	@Test
+	public void shouldBeLastNumberWhenSettingLongNumberTwice() {
+		registry.set("type", "key", 123);
+		registry.set("type", "key", 1234);
+		assertThat(registry.get("type", "key")).isEqualTo(1234);
+	}
+
+	@Test
+	public void shouldBeLastNumberWhenSettingGaugeNumberTwice() {
+		registry.set("type", "key", () -> 123);
+		registry.set("type", "key", () -> 1234);
+		assertThat(registry.get("type", "key")).isEqualTo(1234);
+	}
+
+	@Test
+	public void shouldBeLastNumberWhenSettingGaugeNumberAndLongNumber() {
+		registry.set("type", "key", () -> 123);
+		registry.set("type", "key", 1234);
+		assertThat(registry.get("type", "key")).isEqualTo(1234);
+	}
+
+	@Test
+	public void shouldBeLastNumberWhenSettingLongNumberAndGaugeNumber() {
+		registry.set("type", "key", 123);
+		registry.set("type", "key", () -> 1234);
+		assertThat(registry.get("type", "key")).isEqualTo(1234);
+	}
+
+	@Test
+	public void shouldBeNextNumberWhenGettingGaugeNumberTwice() {
+		registry.set("type", "key", () -> {
+			registry.increment("type", "key2");
+			return registry.get("type", "key2");
+		});
+		assertThat(registry.get("type", "key")).isEqualTo(1);
+		assertThat(registry.get("type", "key")).isEqualTo(2);
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenIncrementingGaugeNumber() {
+		registry.set("type", "key", () -> 123);
+		Throwable throwable = null;
+		try {
+			registry.increment("type", "key");
+		} catch (Exception ex) {
+			throwable = ex;
+		}
+		assertThat(throwable).isNotNull()
+				.hasMessageEndingWith("cannot be cast to java.util.concurrent.atomic.LongAdder");
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenDecrementingGaugeNumber() {
+		registry.set("type", "key", () -> 123);
+		Throwable throwable = null;
+		try {
+			registry.decrement("type", "key");
+		} catch (Exception ex) {
+			throwable = ex;
+		}
+		assertThat(throwable).isNotNull()
+				.hasMessageEndingWith("cannot be cast to java.util.concurrent.atomic.LongAdder");
+	}
+
+	@Test
+	public void shouldThrowExceptionWhenAddingToGaugeNumber() {
+		registry.set("type", "key", () -> 123);
+		Throwable throwable = null;
+		try {
+			registry.add("type", "key", 123);
+		} catch (Exception ex) {
+			throwable = ex;
+		}
+		assertThat(throwable).isNotNull()
+				.hasMessageEndingWith("cannot be cast to java.util.concurrent.atomic.LongAdder");
+	}
+
+	@Test
+	public void shouldBeSameReferenceWhenGettingInstance() {
+		MetricRegistry metricRegistry = MetricRegistry.getInstance();
+		assertThat(metricRegistry == registry).isEqualTo(true);
+	}
+}
--- metrics-jetty/src/main/java/com/tqdev/metrics/jetty/InstrumentedHandler.java
+++ metrics-jetty/src/main/java/com/tqdev/metrics/jetty/InstrumentedHandler.java
@@ -28,11 +28,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.server.AsyncContextState;
 import org.eclipse.jetty.server.HttpChannelState;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.HandlerWrapper;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.springframework.http.HttpMethod;
 
 import com.tqdev.metrics.core.Gauge;
 import com.tqdev.metrics.core.MetricRegistry;
@@ -62,7 +63,6 @@ public class InstrumentedHandler extends HandlerWrapper {
 
 		@Override
 		public void onTimeout(AsyncEvent event) throws IOException {
-			registry.increment("jetty.Other.Counters", "async-timeouts");
 		}
 
 		@Override
@@ -73,7 +73,6 @@ public class InstrumentedHandler extends HandlerWrapper {
 
 		@Override
 		public void onError(AsyncEvent event) throws IOException {
-			registry.increment("jetty.Other.Counters", "async-errors");
 		}
 
 		@Override
@@ -82,9 +81,6 @@ public class InstrumentedHandler extends HandlerWrapper {
 			final HttpServletRequest request = (HttpServletRequest) state.getRequest();
 			final HttpServletResponse response = (HttpServletResponse) state.getResponse();
 			updateResponses(request, response, startTime);
-			if (state.getHttpChannelState().getState() != HttpChannelState.State.DISPATCHED) {
-				registry.decrement("jetty.Other.Gauges", "active-suspended");
-			}
 		}
 	};
 
@@ -97,12 +93,6 @@ public class InstrumentedHandler extends HandlerWrapper {
 	protected void doStart() throws Exception {
 		super.doStart();
 
-		registry.set("jetty.Other.Counters", "async-timeouts", 0);
-		registry.set("jetty.Other.Counters", "async-errors", 0);
-		registry.set("jetty.Other.Gauges", "active-suspended", 0);
-		registry.set("jetty.Other.Gauges", "active-dispatches", 0);
-		registry.set("jetty.Other.Gauges", "active-requests", 0);
-		registry.set("jetty.Other.Gauges", "async-dispatches", 0);
 		for (int responseStatus = 1; responseStatus <= 5; responseStatus++) {
 			registry.set("jetty.Response.Invocations", responseStatus + "xx-responses", 0);
 			registry.set("jetty.Response.Durations", responseStatus + "xx-responses", 0);
@@ -144,22 +134,15 @@ public class InstrumentedHandler extends HandlerWrapper {
 	public void handle(String path, Request request, HttpServletRequest httpRequest, HttpServletResponse httpResponse)
 			throws IOException, ServletException {
 
-		registry.increment("jetty.Other.Gauges", "active-dispatches");
-
 		final long start;
 		final HttpChannelState state = request.getHttpChannelState();
 		if (state.isInitial()) {
 			// new request
-			registry.increment("jetty.Other.Gauges", "active-requests");
 			start = request.getTimeStamp() * 1000000;
 			state.addListener(listener);
 		} else {
 			// resumed request
 			start = System.currentTimeMillis() * 1000000;
-			registry.decrement("jetty.Other.Gauges", "active-suspended");
-			if (state.getState() == HttpChannelState.State.DISPATCHED) {
-				registry.increment("jetty.Other.Gauges", "async-dispatches");
-			}
 		}
 
 		try {
@@ -167,13 +150,10 @@ public class InstrumentedHandler extends HandlerWrapper {
 		} finally {
 			final long duration = System.currentTimeMillis() * 1000000 - start;
 
-			registry.decrement("jetty.Other.Gauges", "active-dispatches");
 			registry.increment("jetty.Aggregated.Invocations", "dispatches");
 			registry.add("jetty.Aggregated.Durations", "dispatches", duration);
 
-			if (state.isSuspended()) {
-				registry.increment("jetty.Other.Gauges", "active-suspended");
-			} else if (state.isInitial()) {
+			if (!state.isSuspended() && state.isInitial()) {
 				updateResponses(httpRequest, httpResponse, start);
 			}
 			// else onCompletion will handle it.
@@ -221,7 +201,6 @@ public class InstrumentedHandler extends HandlerWrapper {
 	 *            the start
 	 */
 	private void updateResponses(HttpServletRequest request, HttpServletResponse response, long start) {
-		registry.decrement("jetty.Other.Gauges", "active-requests");
 		final long duration = System.currentTimeMillis() * 1000000 - start;
 		registry.increment("jetty.Aggregated.Invocations", "requests");
 		registry.add("jetty.Aggregated.Durations", "requests", duration);
