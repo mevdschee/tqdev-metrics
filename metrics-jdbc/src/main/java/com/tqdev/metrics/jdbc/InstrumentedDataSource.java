6de1b300464c2325333a17ea87457037da1e3386
Maurits van der Schee <maurits@vdschee.nl>
2017-11-17 21:21:11 +0100

    spaces or tabs?

--- metrics-jdbc/src/main/java/com/tqdev/metrics/jdbc/InstrumentedDataSource.java
+++ metrics-jdbc/src/main/java/com/tqdev/metrics/jdbc/InstrumentedDataSource.java
@@ -1,8 +1,5 @@
 package com.tqdev.metrics.jdbc;
 
-import com.tqdev.metrics.core.MetricRegistry;
-
-import javax.sql.DataSource;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,83 +10,87 @@ import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.logging.Logger;
 
+import javax.sql.DataSource;
+
+import com.tqdev.metrics.core.MetricRegistry;
+
 public class InstrumentedDataSource implements DataSource, Closeable {
-    private DataSource wrapped;
-    private MetricRegistry registry;
+	private DataSource wrapped;
+	private MetricRegistry registry;
 
-    private volatile boolean metricsEnabled = false;
+	private volatile boolean metricsEnabled = false;
 
-    public InstrumentedDataSource(DataSource wrapped, MetricRegistry registry) {
-        this.wrapped = wrapped;
-        this.registry = registry;
-    }
+	public InstrumentedDataSource(DataSource wrapped, MetricRegistry registry) {
+		this.wrapped = wrapped;
+		this.registry = registry;
+	}
 
-    public void setMetricsEnabled(boolean enabled) {
-        metricsEnabled = enabled;
-    }
+	public void setMetricsEnabled(boolean enabled) {
+		metricsEnabled = enabled;
+	}
 
-    @Override
-    public Connection getConnection() throws SQLException {
-        if (!metricsEnabled) {
-            return wrapped.getConnection();
-        }
-        return new InstrumentedConnection(wrapped.getConnection(), registry);
-    }
+	@Override
+	public Connection getConnection() throws SQLException {
+		if (!metricsEnabled) {
+			return wrapped.getConnection();
+		}
+		return new InstrumentedConnection(wrapped.getConnection(), registry);
+	}
 
-    @Override
-    public Connection getConnection(String username, String password) throws SQLException {
-        if (!metricsEnabled) {
-            return wrapped.getConnection();
-        }
-        return new InstrumentedConnection(wrapped.getConnection(username, password), registry);
-    }
+	@Override
+	public Connection getConnection(String username, String password) throws SQLException {
+		if (!metricsEnabled) {
+			return wrapped.getConnection();
+		}
+		return new InstrumentedConnection(wrapped.getConnection(username, password), registry);
+	}
 
-    @Override
-    public PrintWriter getLogWriter() throws SQLException {
-        return wrapped.getLogWriter();
-    }
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		return wrapped.getLogWriter();
+	}
 
-    @Override
-    public void setLogWriter(PrintWriter out) throws SQLException {
-        wrapped.setLogWriter(out);
-    }
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		wrapped.setLogWriter(out);
+	}
 
-    @Override
-    public void setLoginTimeout(int seconds) throws SQLException {
-        wrapped.setLoginTimeout(seconds);
-    }
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		wrapped.setLoginTimeout(seconds);
+	}
 
-    @Override
-    public int getLoginTimeout() throws SQLException {
-        return wrapped.getLoginTimeout();
-    }
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		return wrapped.getLoginTimeout();
+	}
 
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return wrapped.getParentLogger();
-    }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return wrapped.getParentLogger();
+	}
 
-    @Override
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-        return wrapped.unwrap(iface);
-    }
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return wrapped.unwrap(iface);
+	}
 
-    @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        return wrapped.isWrapperFor(iface);
-    }
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return wrapped.isWrapperFor(iface);
+	}
 
-    @Override
-    public void close() throws IOException {
-        if (wrapped instanceof Closeable) {
-            ((Closeable) wrapped).close();
-        } else {
-            try {
-                Method close = wrapped.getClass().getMethod("close");
-                close.invoke(wrapped);
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                // Ignore, no parameterless close method defined
-            }
-        }
-    }
+	@Override
+	public void close() throws IOException {
+		if (wrapped instanceof Closeable) {
+			((Closeable) wrapped).close();
+		} else {
+			try {
+				Method close = wrapped.getClass().getMethod("close");
+				close.invoke(wrapped);
+			} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+				// Ignore, no parameterless close method defined
+			}
+		}
+	}
 }
