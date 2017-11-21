0d0076d87e90cdc06ba43939c2286fb0f5471c78
Maurits van der Schee <maurits@vdschee.nl>
2017-11-21 21:04:02 +0100

    Added all relevant tests, some fail due to bad implementation

--- metrics-jdbc/src/test/java/com/tqdev/metrics/jdbc/InstrumentedDataSourceTest.java
+++ metrics-jdbc/src/test/java/com/tqdev/metrics/jdbc/InstrumentedDataSourceTest.java
@@ -22,8 +22,10 @@ package com.tqdev.metrics.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -42,116 +44,68 @@ public class InstrumentedDataSourceTest extends InstrumentedDataSourceTestBase {
 	}
 
 	@Test
-	public void shouldMeasureQueryWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("select").executeQuery();
-		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(123456789);
+	public void shouldMeasurePreparedStatement() throws SQLException {
+		PreparedStatement statements[] = { dataSource.getConnection().prepareStatement("select"),
+				dataSource.getConnection().prepareStatement("select", 1),
+				dataSource.getConnection().prepareStatement("select", new int[] {}),
+				dataSource.getConnection().prepareStatement("select", new String[] {}),
+				dataSource.getConnection().prepareStatement("select", 1, 1),
+				dataSource.getConnection().prepareStatement("select", 1, 1, 1) };
+		for (PreparedStatement statement : statements) {
+			statement.execute();
+			statement.executeQuery();
+			statement.executeUpdate();
+			statement.executeLargeUpdate();
+			statement.addBatch();
+			statement.executeBatch();
+			statement.executeLargeBatch();
+		}
+		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(1L * 6 * statements.length);
+		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(123456789L * 6 * statements.length);
 	}
 
 	@Test
-	public void shouldMeasureQueryStringWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("select1").executeQuery("select2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "select2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "select2")).isEqualTo(123456789);
+	public void shouldMeasureCallableStatement() throws SQLException {
+		CallableStatement statements[] = { dataSource.getConnection().prepareCall("select"),
+				dataSource.getConnection().prepareCall("select", 1, 1),
+				dataSource.getConnection().prepareCall("select", 1, 1, 1) };
+		for (CallableStatement statement : statements) {
+			statement.execute();
+			statement.executeQuery();
+			statement.executeUpdate();
+			statement.executeLargeUpdate();
+			statement.addBatch();
+			statement.executeBatch();
+			statement.executeLargeBatch();
+		}
+		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(1L * 6 * statements.length);
+		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(123456789L * 6 * statements.length);
 	}
 
 	@Test
-	public void shouldMeasureQueryTwiceWithPreparedStatement() throws SQLException {
-		PreparedStatement statement = dataSource.getConnection().prepareStatement("select");
-		statement.executeQuery();
-		statement.executeQuery();
-		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(2);
-		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(246913578);
+	public void shouldMeasureStatement() throws SQLException {
+		Statement statements[] = { dataSource.getConnection().createStatement(),
+				dataSource.getConnection().createStatement(1, 1), dataSource.getConnection().createStatement(1, 1, 1) };
+		for (Statement statement : statements) {
+			statement.execute("select");
+			statement.execute("select", 1);
+			statement.execute("select", new int[] {});
+			statement.execute("select", new String[] {});
+			statement.executeQuery("select");
+			statement.executeUpdate("select");
+			statement.executeUpdate("select", 1);
+			statement.executeUpdate("select", new int[] {});
+			statement.executeUpdate("select", new String[] {});
+			statement.executeLargeUpdate("select");
+			statement.executeLargeUpdate("select", 1);
+			statement.executeLargeUpdate("select", new int[] {});
+			statement.executeLargeUpdate("select", new String[] {});
+			statement.addBatch("select");
+			statement.executeBatch();
+			statement.executeLargeBatch();
+		}
+		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(1L * 15 * statements.length);
+		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(123456789L * 15 * statements.length);
 	}
 
-	@Test
-	public void shouldMeasureUpdateWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("update").executeUpdate();
-		assertThat(registry.get("jdbc.Statement.Invocations", "update")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "update")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureUpdateStringWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("update1").executeUpdate("update2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "update2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "update2")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureExecuteWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("delete").execute();
-		assertThat(registry.get("jdbc.Statement.Invocations", "delete")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "delete")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureExecuteStringWithPreparedStatement() throws SQLException {
-		dataSource.getConnection().prepareStatement("delete1").execute("delete2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "delete2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "delete2")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureQueryWithStatement() throws SQLException {
-		dataSource.getConnection().createStatement().executeQuery("select");
-		assertThat(registry.get("jdbc.Statement.Invocations", "select")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "select")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureUpdateWithStatement() throws SQLException {
-		dataSource.getConnection().createStatement().executeUpdate("update");
-		assertThat(registry.get("jdbc.Statement.Invocations", "update")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "update")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureExecuteWithStatement() throws SQLException {
-		dataSource.getConnection().createStatement().execute("delete");
-		assertThat(registry.get("jdbc.Statement.Invocations", "delete")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "delete")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureQueryWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call").executeQuery();
-		assertThat(registry.get("jdbc.Statement.Invocations", "call")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureQueryStringWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call1").executeQuery("call2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "call2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call2")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureUpdateWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call").executeUpdate();
-		assertThat(registry.get("jdbc.Statement.Invocations", "call")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureUpdateStringWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call1").executeUpdate("call2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "call2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call2")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureExecuteWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call").execute();
-		assertThat(registry.get("jdbc.Statement.Invocations", "call")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call")).isEqualTo(123456789);
-	}
-
-	@Test
-	public void shouldMeasureExecuteStringWithCallableStatement() throws SQLException {
-		dataSource.getConnection().prepareCall("call1").execute("call2");
-		assertThat(registry.get("jdbc.Statement.Invocations", "call2")).isEqualTo(1);
-		assertThat(registry.get("jdbc.Statement.Durations", "call2")).isEqualTo(123456789);
-	}
 }
